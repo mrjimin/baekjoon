52820df4c9f91a077d3b2fa1d87061203c05abd7
mrjimin <aa090402@naver.com>
2026-03-08 21:32:24 +0900

    feat: time(tips)

--- tips.kt
+++ tips.kt
@@ -11,3 +11,8 @@ val format2 = DecimalFormat("##00.000")
 list.sort() // 오름차순 > [1, 2, 3, 4, 5]
 list.sortDescending() // 내림차순 > [5, 4, 3, 2, 1]
 list.reverse() // 리스트 뒤집기 > [1, 2, 3, 4, 5]
+
+// 시간 구하기
+val totalMinutes = (h * 60 + m) % (24 * 60)
+val hours = totalMinutes / 60
+val minutes = totalMinutes % 60
